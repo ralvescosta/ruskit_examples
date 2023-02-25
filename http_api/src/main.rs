887fc7bff52f84a84ffc89973b725beb9bcb7364
ralvescosta <rafael.rac.mg@gmail.com>
2023-02-24 21:42:08 -0300

    feat: http api

--- http_api/src/main.rs
+++ http_api/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+mod controllers;
+mod openapi;
+mod routes;
+mod viewmodels;
+
+use env::{ConfigBuilder, Configs, Empty};
+use httpw::server::HttpwServerImpl;
+use openapi::ApiDoc;
+use std::error::Error;
+use utoipa::OpenApi;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let cfg = default_setup().await?;
+
+    let doc = ApiDoc::openapi();
+
+    let server = HttpwServerImpl::new(&cfg.app)
+        .register(routes::todos_routes())
+        .openapi(&doc);
+
+    server.start().await?;
+
+    Ok(())
+}
+
+async fn default_setup() -> Result<Configs<Empty>, Box<dyn Error>> {
+    let (app_cfg, mut builder) = ConfigBuilder::new()
+        .load_from_aws_secret()
+        .otlp()
+        .laze_load();
+
+    logging::setup(&app_cfg)?;
+
+    let cfg = builder.build::<Empty>().await?;
+
+    traces::otlp::setup(&cfg)?;
+    metrics::otlp::setup(&cfg)?;
+
+    Ok(cfg)
 }
